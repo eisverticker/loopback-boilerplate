3dc76b58eb4a9ab6fa03f3d8472c547c740c3e96
eisverticker <eisverticker@gmail.com>
2016-08-18 15:03:47 +0200

    u.A. config verbessert (hierachical rbac)

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -3,10 +3,17 @@ var path = require('path');
 
 module.exports = function(user) {
 
-  var senderMail = 'eisverticker@gmail.com';
-  /*user.afterRemote('create', function(context, user, next){
-    console.log("Registrierung!" + JSON.stringify(context));
-  })*/
+  user.afterRemote('confirm', function(context, result, next){
+
+    //if the user ist confirmed he will get the first role in the list
+    var role = config.custom.roles[0];
+
+    user.app.models.AppUser.addRole(context.req.query.uid, role, function(err){
+      if(err) throw err;
+
+      return next();
+    });
+  });
 
   //send verification email after registration
   user.afterRemote('create', function(context, user, next) {
@@ -46,5 +53,174 @@ module.exports = function(user) {
       console.log('> sending password reset email to:', info.email);
     });
   });
+
+  /**
+   * Get all Roles (name and id) of a user (by userId)
+   */
+  user.roles = function(id, cb) {
+    user.app.models.RoleMapping.find({
+      where: {
+        'principalId': id,
+        'principalType': 'USER'
+      },
+      include: ['role']
+    }, function(err, roles){
+
+        cb(null,
+          roles.map(
+            function(role){
+              var roleObj = role.toJSON();
+              return {
+                id: roleObj.role.id,
+                name: roleObj.role.name
+              };
+            }
+          )
+        );
+      }
+    );
+  }
+
+  user.remoteMethod(
+      'roles',
+      {
+        accessType: 'WRITE',
+        accepts: {arg: 'id', type: 'string'},
+        returns: {arg: 'data', type: 'string'},
+        http: {path: '/:id/roles', verb: 'get'}
+      }
+  );
+
+
+  /**
+   * Add the user to the given role by name.
+   * (original source: https://gist.github.com/leftclickben/aa3cf418312c0ffcc547)
+   * @param {string} roleName
+   * @param {Function} callback
+   */
+  user.addRole = function(id, rolename, callback) {
+      var Role = user.app.models.Role;
+      var RoleMapping = user.app.models.RoleMapping;
+
+      var error, userId = id;
+      Role.findOne(
+          {
+              where: { name: rolename }
+          },
+          function(err, role) {
+              if (err) {
+                  return callback(err);
+              }
+
+              if (!role) {
+                  error = new Error('Role ' + rolename + ' not found.');
+                  error['http_code'] = 404;
+                  return callback(error);
+              }
+
+              RoleMapping.findOne(
+                  {
+                      where: {
+                          principalType: RoleMapping.USER,
+                          principalId: userId,
+                          roleId: role.id
+                      }
+                  },
+                  function(err, roleMapping) {
+                      if (err) {
+                          return callback(err);
+                      }
+
+                      if (roleMapping) {
+                          return callback();
+                      }
+                      role.principals.create(
+                          {
+                              principalType: RoleMapping.USER,
+                              principalId: userId
+                          },
+                          callback
+                      );
+                  }
+              );
+          }
+      );
+  };
+  user.remoteMethod(
+      'addRole',
+      {
+        accepts: [
+          {arg: 'id', type: 'string'},
+          {arg: 'rolename', type: 'string' }
+        ],
+        http: {path: '/:id/roles', verb: 'put'}
+      }
+  );
+
+  /**
+   * Remove the user from the given role by name.
+   * (original source: https://gist.github.com/leftclickben/aa3cf418312c0ffcc547)
+   *
+   * @param {string} roleName
+   * @param {Function} callback
+   */
+  user.removeRole = function(id, rolename, callback) {
+    var Role = user.app.models.Role;
+    var RoleMapping = user.app.models.RoleMapping;
+
+      var error, userId = id;
+      console.log("delete", rolename);
+      console.log("from", id);
+      Role.findOne(
+          {
+              where: { name: rolename }
+          },
+          function(err, roleObj) {
+              if (err) {
+                  return callback(err);
+              }
+
+              if (!roleObj) {
+                  error = new Error('Role ' + rolename + ' not found.');
+                  error['http_code'] = 404;
+                  return callback(error);
+              }
+              console.log(roleObj);
+              RoleMapping.findOne(
+                  {
+                      where: {
+                          principalType: RoleMapping.USER,
+                          principalId: userId,
+                          roleId: roleObj.id
+                      }
+                  },
+                  function(err, roleMapping) {
+                      if (err) {
+                          return callback(err);
+                      }
+
+                      if (!roleMapping) {
+                          return callback();
+                      }
+                      console.log(roleMapping);
+
+                      roleMapping.destroy(callback);
+                  }
+              );
+          }
+      );
+  };
+  user.remoteMethod(
+      'removeRole',
+      {
+          description: 'Remove User to the named role',
+          accessType: 'WRITE',
+          accepts: [
+            {arg: 'id', type: 'string' },
+            {arg: 'rolename', type: 'string' }
+          ],
+          http: {path: '/:id/roles/:rolename', verb: 'delete'}
+      }
+  );
 
 };
--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,3 +1,5 @@
+var config = require('../config.json');
+
 module.exports = function(server){
   var User = server.models.AppUser,
       Role = server.models.Role,
@@ -8,45 +10,80 @@ module.exports = function(server){
    * then create the role administrator
    * then assign this role to user Admin
    */
-  console.log("findOrCreate");
-  var adminUserName = 'Administrator';
-  User.findOrCreate(
-    {
-      where: {
-        'username': adminUserName
-      }
-    },{
-      username: adminUserName,
-      email: 'foobar3@no-spam.ws',
-      password: '+rA4uswatHaP',
-      emailVerified: true
-    }, function(err, user){
-      if(err) throw err;
-      console.log('Created User:', user);
+  var roles = config.custom.rbac.roles.slice();
+  var isHierachical = config.custom.rbac.hierachical;
+
+  var createRoles = function(roles, lastRoleObj, callback){
+    if(roles.length > 0){
+
+      var roleName = roles.pop();
 
       Role.findOrCreate({
         where: {
-          name: 'administrator'
+          "name": roleName
         }
       },{
-        name: 'administrator'
-      },function(err, role){
-        if(err) throw err;
-        console.log('Created role:', role);
+        "name": roleName
+      }, function(err, roleObj){
+        if(err) return err;
 
-        role.principals.create({
-          principalType: RoleMapping.USER,
-          principalId: user.id
-        }, function(err, principal){
-          if(err) throw err;
-          console.log('Created principal:', principal);
-          /*Role.isInRole('administrator', ctx, function(){
-            console.log("wwwoowww");
-          });*/
-        });
+        if(lastRoleObj !== null && isHierachical){
+          lastRoleObj.principals.create(
+              {
+                  principalType: RoleMapping.ROLE,
+                  principalId: roleObj.id
+              },
+              function(err){
+                if(err) throw err;
+
+                createRoles(roles, roleObj, callback);
+              }
+          );
+        }else{
+          createRoles(roles, roleObj, callback);
+        }
+
       });
-    }
-  );
 
+    }else{
+
+      callback();
+
+    }
+  };
+
+  createRoles(roles, null, function(){
+
+    User.findOne({
+      where: {
+        'username': config.custom.admin.username
+      }
+    }, function(err, user){
+      if(err) throw err;
+
+      if(!user){
+
+        User.create(
+          {
+            username: config.custom.admin.username,
+            email: config.custom.admin.email,
+            password: config.custom.admin.password,
+            emailVerified: true
+          }, function(err, user){
+            if(err) throw err;
+
+            User.addRole(user.id, 'administrator', function(err){
+              if(err) throw err;
+
+              console.log("Administrator was successfully created");
+            });
+
+          }
+        );
+
+      }
+    });
+
+  });
 
 }
